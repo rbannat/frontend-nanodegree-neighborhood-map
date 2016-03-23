73e1594d72fcc99e7ac2fe0485870a6b9aac678b
Rene Bannat <r.bannat@gmail.com>
2016-03-23 13:01:06 +0100

    add initial locations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,29 @@
+var initialLocations = [
+  {
+    name: 'Berlin'
+  },
+  {
+    name: 'London'
+  }
+];
+
+var Location = function (data) {
+  this.name = data.name;
+};
+
 var ViewModel = function () {
   var self = this;
 
-  self.locations = ko.observableArray([
-    {name: "Bungle"},
-    {name: "George"},
-    {name: "Zippy"}
-  ]);
+  self.locations = ko.observableArray([]);
+
+  initialLocations.forEach(function (location) {
+    self.locations.push(new Location(location));
+  });
+
+
+  //filteredLocations
+
+
 };
 
 ko.applyBindings(new ViewModel());
